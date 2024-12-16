e210b0f95f95170bb7b5b8a77c495e47e5f14ff8
Zhivko Shopov <zhishopov@gmail.com>
2024-12-16 17:06:38 +0000

    Sum table rows

--- JS-Advanced/DOM/06.Sum Table/SumTable.js
+++ JS-Advanced/DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+  const rows = Array.from(document.querySelectorAll("tr")).slice(1, -1);
+  const result = document.getElementById("sum");
+  let sum = 0;
 
+  rows.forEach((row) => {
+    let value = Number(row.lastElementChild.textContent);
+    sum += value;
+  });
+
+  result.textContent = sum;
 }
