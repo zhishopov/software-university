673c9611edf27d5e276eeb5f767d8270ed55674c
Zhivko Shopov <zhishopov@gmail.com>
2024-12-16 16:38:32 +0000

    Collect list items solution

--- JS-Advanced/DOM/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/DOM/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+  const list = document.getElementById("items");
+  const textarea = document.getElementById("result");
+  const items = Array.from(list.children);
+
+  const itemsText = items.map((li) => li.textContent).join("\n");
+  textarea.value = itemsText;
 }
