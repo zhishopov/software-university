a5a027787e7f8e42eec85c506b29a361d169b48b
Zhivko <zhishopov@gmail.com>
2024-12-17 23:42:55 +0000

    Search in List solution

--- JS-Advanced/DOM/Exercises/04. Search in List/search.js
+++ JS-Advanced/DOM/Exercises/04. Search in List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+   const towns = Array.from(document.querySelectorAll('li'));
+   const searchText = document.getElementById('searchText').value;
+   const result = document.getElementById('result');
+   let count = 0;
+
+   for(let town of towns) {
+      if(town.textContent.includes(searchText)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = "underline";
+         count++;
+      }
+   }
+   result.textContent = `${count} matches found`
 }
