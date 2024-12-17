64e97636d29ac05328cba28fb0dc15b2f9a4c2b0
Zhivko <zhishopov@gmail.com>
2024-12-17 01:28:20 +0000

    Pascal or Camel case solution

--- JS-Advanced/DOM/Exercises/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/DOM/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById("text").value;
+  const convention = document.getElementById("naming-convention").value;
+  const divResult = document.getElementById('result');
+  const words = text.toLowerCase().split(" ");
+  let result = "";
+
+  if (convention === "Camel Case") {
+    result = words[0]; // First word remains lowercase
+    for (let i = 1; i < words.length; i++) {
+      if (words[i]) {
+        result += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+    }
+  } else if (convention === "Pascal Case") {
+    for (let word of words) {
+      if (word) {
+        result += word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    }
+  } else {
+    result = "Error!";
+  }
+
+  divResult.textContent = result;
 }
