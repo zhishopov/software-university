4e12ecc22cb62f8d062ad3ecc53c89e481383aca
Zhivko Shopov <zhishopov@gmail.com>
2024-12-18 17:20:20 +0000

    Add delete button solution

--- JS-Advanced/DOM-Events/Lab/03. Add-Delete/app.js
+++ JS-Advanced/DOM-Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+  const list = document.getElementById("items");
+  const input = document.getElementById("newItemText").value;
+
+  const newItem = document.createElement("li");
+  newItem.textContent = input;
+
+  const deleteBtn = document.createElement("a");
+  deleteBtn.href = "#";
+  deleteBtn.textContent = "[Delete]";
+
+  deleteBtn.addEventListener("click", removeItem);
+
+  function removeItem() {
+    newItem.remove();
+  }
+  newItem.appendChild(deleteBtn);
+
+  list.appendChild(newItem);
 }
