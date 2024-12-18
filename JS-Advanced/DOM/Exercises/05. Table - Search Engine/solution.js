631420e81f880f606e46e3c75da2072b86b1701d
Zhivko Shopov <zhishopov@gmail.com>
2024-12-18 16:40:14 +0000

    Table search solution

--- JS-Advanced/DOM/Exercises/05. Table - Search Engine/solution.js
+++ JS-Advanced/DOM/Exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    let rows = Array.from(document.querySelectorAll("tbody tr"));
+    let match = document.getElementById("searchField").value;
 
-   }
+    for (let row of rows) {
+      if (row.textContent.includes(match)) {
+        row.classList.add("select");
+      } else {
+        row.classList.remove("select");
+      }
+    }
+    match.textContent = "";
+  }
 }
