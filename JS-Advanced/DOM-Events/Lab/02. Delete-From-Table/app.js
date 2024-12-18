b3978cdf69a703722907d86913c9b9b2fb27d6b5
Zhivko Shopov <zhishopov@gmail.com>
2024-12-18 17:15:36 +0000

    Delete from table solution

--- JS-Advanced/DOM-Events/Lab/02. Delete-From-Table/app.js
+++ JS-Advanced/DOM-Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const input = document.querySelector('input[name="email"]').value;
+  const result = document.getElementById("result");
+  const rows = Array.from(document.querySelectorAll("#customers tbody tr"));
+
+  let deleted = false;
+
+  for (let row of rows) {
+    const column = row.children[1];
+
+    if (column.textContent === input) {
+      row.remove();
+      deleted = true;
+    }
+  }
+  if (deleted) {
+    result.textContent = "Deleted.";
+  } else {
+    result.textContent = "Not found.";
+  }
 }
