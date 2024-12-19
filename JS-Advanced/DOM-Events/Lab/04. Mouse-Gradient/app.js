ca812fa77b82b7ab2eaf43864243e09d49134ce0
Zhivko Shopov <zhishopov@gmail.com>
2024-12-19 17:57:24 +0000

    Mouse gradient solution

--- JS-Advanced/DOM-Events/Lab/04. Mouse-Gradient/app.js
+++ JS-Advanced/DOM-Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const gradient = document.getElementById("gradient");
+  const result = document.getElementById("result");
+
+  gradient.addEventListener("mousemove", onMouseMove);
+
+  function onMouseMove(event) {
+    let x = event.offsetX;
+    let percent = Math.floor((x / 300) * 100);
+    result.textContent = `${percent}%`;
+  }
 }
