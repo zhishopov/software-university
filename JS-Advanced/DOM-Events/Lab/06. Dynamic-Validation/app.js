8a309743991e3ce6e83719bfa507705bc48bfcfc
Zhivko Shopov <zhishopov@gmail.com>
2024-12-19 19:10:06 +0000

    Dynamic validation solution

--- JS-Advanced/DOM-Events/Lab/06. Dynamic-Validation/app.js
+++ JS-Advanced/DOM-Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+  const input = document.getElementById("email");
+  const match = /^[a-z]+@[a-z]+.[a-z]+$/;
+
+  input.addEventListener("change", () => {
+    input.className = "";
+    if (!match.test(input.value)) {
+      input.className = "error";
+    }
+  });
 }
