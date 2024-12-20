14fc6f91a47416a17025d820e859e9c2c09600bb
Zhivko <zhishopov@gmail.com>
2024-12-20 03:17:54 +0000

    Sections solution

--- JS-Advanced/DOM-Events/Exercises/01. Sections/script.js
+++ JS-Advanced/DOM-Events/Exercises/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   const result = document.getElementById('content');
+
+   for(let word of words) {
+      const newDiv = document.createElement('div');
+      const newParagraph = document.createElement('p');
+
+      newParagraph.textContent = word;
+      newParagraph.style.display = 'none';
+      newDiv.appendChild(newParagraph);
+      
+      newDiv.addEventListener('click', () => {
+         newParagraph.style.display = 'block';
+      });
+      result.appendChild(newDiv);
+   }
+
 }
