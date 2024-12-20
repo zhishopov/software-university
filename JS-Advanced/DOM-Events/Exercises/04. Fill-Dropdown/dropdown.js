c106cd27648319aaeb2b00585883c9eae7dcf18b
Zhivko <zhishopov@gmail.com>
2024-12-20 03:58:09 +0000

    Fill dropdown solution

--- JS-Advanced/DOM-Events/Exercises/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/DOM-Events/Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+  const menu = document.getElementById("menu");
+  const inputText = document.getElementById("newItemText");
+  const valueText = document.getElementById("newItemValue");
+
+  const newOption = document.createElement("option");
+  newOption.textContent = inputText.value;
+  newOption.value = valueText.value;
+  menu.appendChild(newOption);
+
+  inputText.value = "";
+  valueText.value = "";
 }
