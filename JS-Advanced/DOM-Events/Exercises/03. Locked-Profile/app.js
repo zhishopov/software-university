236950dac87e0a7240a12ed551506275462ad40a
Zhivko <zhishopov@gmail.com>
2024-12-20 03:44:27 +0000

    Locked profile solution

--- JS-Advanced/DOM-Events/Exercises/03. Locked-Profile/app.js
+++ JS-Advanced/DOM-Events/Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,16 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const profiles = Array.from(document.querySelectorAll('.profile'));
+
+    profiles.forEach(profile => {
+        const button = profile.querySelector('button');
+        const unlockRadio = profile.querySelector('input[value="unlock"]');
+
+        button.addEventListener('click', () => {
+            if(unlockRadio.checked) {
+                const hiddenInfo = profile.querySelector('div');
+                hiddenInfo.style.display = hiddenInfo.style.display === 'block' ? 'none' : 'block';
+                button.textContent = button.textContent === 'Show more' ? 'Hide it' : 'Show more';
+            }
+        })
+    })
 }
