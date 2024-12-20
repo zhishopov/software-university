17659d8e149956c70d149d3a09e861c8f5b181b6
Zhivko Shopov <zhishopov@gmail.com>
2024-12-20 20:49:42 +0000

    Furniture solution

--- JS-Advanced/DOM-Events/Exercises/06. Furniture/solution.js
+++ JS-Advanced/DOM-Events/Exercises/06. Furniture/solution.js
@@ -1,4 +1,86 @@
 function solve() {
+  let generateBtn = document.querySelector("button");
+  let buyBtn = Array.from(document.querySelectorAll("button"))[1];
 
-  //TODO...
+  const tableBody = document.querySelector("tbody");
+  generateBtn.addEventListener("click", generate);
+  buyBtn.addEventListener("click", buy);
+
+  function generate() {
+    let textArea = document.querySelector("textarea").value;
+
+    let parsedArray = JSON.parse(textArea);
+
+    for (let furniture of parsedArray) {
+      let tableRowElement = document.createElement("tr");
+
+      let tdImage = document.createElement("td");
+      let tdName = document.createElement("td");
+      let tdPrice = document.createElement("td");
+      let tdFactor = document.createElement("td");
+      let tdMark = document.createElement("td");
+
+      tableRowElement.appendChild(tdImage);
+      tableRowElement.appendChild(tdName);
+      tableRowElement.appendChild(tdPrice);
+      tableRowElement.appendChild(tdFactor);
+      tableRowElement.appendChild(tdMark);
+
+      let image = document.createElement("img");
+      image.src = furniture.img;
+      tdImage.appendChild(image);
+
+      tdName.innerText = furniture.name;
+      tdPrice.innerText = furniture.price;
+      tdFactor.innerText = furniture.decFactor;
+
+      let checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+
+      tdMark.appendChild(checkBox);
+
+      tableBody.appendChild(tableRowElement);
+    }
+  }
+
+  function buy() {
+    let resultTextArea = Array.from(document.querySelectorAll("textarea"))[1];
+    let output = [];
+
+    let toBuy = Array.from(document.querySelectorAll('input[type="checkbox"]'))
+      .filter((e) => e.checked === true)
+      .map((e) => e.parentNode.parentNode);
+
+    let bought = toBuy
+      .map((e) => Array.from(e.querySelectorAll("td"))[1].innerText)
+      .join(", ");
+
+    output.push(`Bought furniture: ${bought}`);
+
+    let totalPrice = toBuy
+      .map((e) => Array.from(e.querySelectorAll("td"))[2].innerText)
+      .map(Number)
+      .reduce((acc, current) => acc + current, 0);
+
+    output.push(`Total price: ${totalPrice.toFixed(2)}`);
+
+    let averageFactor =
+      toBuy
+        .map((e) => Array.from(e.querySelectorAll("td"))[3].innerText)
+        .map(Number)
+        .reduce((acc, current) => acc + current, 0) / toBuy.length;
+
+    output.push(`Average decoration factor: ${averageFactor}`);
+
+    resultTextArea.value = output.join("\n");
+  }
 }
+
+/* INPUT:
+[{
+"name": "Sofa", 
+"img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", 
+"price": 150, 
+"decFactor": 1.2
+}]
+*/
