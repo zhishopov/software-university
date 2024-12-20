2d92b0761ee1671032482da74408de5f64dcfe3e
Zhivko Shopov <zhishopov@gmail.com>
2024-12-20 20:22:05 +0000

    Encode/Decode solution

--- JS-Advanced/DOM-Events/Exercises/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/DOM-Events/Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const textAreaOne = document.getElementsByTagName("textarea")[0];
+  const textAreaTwo = document.getElementsByTagName("textarea")[1];
+  const encodeButton = document.getElementsByTagName("button")[0];
+  const decodeButton = document.getElementsByTagName("button")[1];
+
+  encodeButton.addEventListener("click", encodeMessage);
+  decodeButton.addEventListener("click", decodeMessage);
+
+  function encodeMessage() {
+    const words = textAreaOne.value;
+    const encoded = [];
+
+    for (let i = 0; i < words.length; i++) {
+      let letter = words[i];
+      letter = String.fromCharCode(letter.charCodeAt(0) + 1);
+      encoded.push(letter);
+    }
+    textAreaTwo.value = encoded.join("");
+    textAreaOne.value = "";
+  }
+
+  function decodeMessage() {
+    const words = textAreaTwo.value;
+    const decoded = [];
+
+    for (let i = 0; i < words.length; i++) {
+      let letter = words[i];
+      letter = String.fromCharCode(letter.charCodeAt(0) - 1);
+      decoded.push(letter);
+    }
+    textAreaOne.value = decoded.join("");
+    textAreaTwo.value = "";
+  }
 }
