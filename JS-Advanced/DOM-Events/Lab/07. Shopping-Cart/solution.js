2807e76dd2fefc7c2e70ffe29e84db30261a103c
Zhivko <zhishopov@gmail.com>
2024-12-20 02:29:03 +0000

    Shopping cart solution

--- JS-Advanced/DOM-Events/Lab/07. Shopping-Cart/solution.js
+++ JS-Advanced/DOM-Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-   //TODO...
+   const buttons = Array.from(document.getElementsByClassName('add-product'));
+   const textarea = document.getElementsByTagName('textarea')[0];
+   const checkoutBtn = document.getElementsByClassName('checkout')[0];
+   const allBtns = Array.from(document.getElementsByTagName('button'));
+   let finalPrice = 0;
+   const list = new Set();
+   
+   for(let button of buttons) {
+      button.addEventListener('click', addProduct);
+   }
+
+   function addProduct(event) {
+      const parent = event.target.parentElement.parentElement;
+      const product = parent.querySelector('.product-details');
+      const price = parent.querySelector('.product-line-price').textContent;
+      const title = product.children[0].textContent;
+
+      textarea.value += `Added ${title} for ${price} to the cart.\n`;
+      finalPrice += Number(price);
+      list.add(title);
+   }
+
+   checkoutBtn.addEventListener('click', calculatePrice);
+
+   function calculatePrice() {
+      textarea.value += `You bought ${Array.from(list).join(', ')} for ${finalPrice.toFixed(2)}.`;
+      allBtns.forEach(button => button.disabled = true);
+   }
 }
