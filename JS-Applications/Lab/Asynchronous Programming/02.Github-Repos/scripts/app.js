8fc9160f69ea35a789ef9dc8f4b50964194e069b
Zhivko Shopov <zhishopov@gmail.com>
2025-01-01 16:52:01 +0000

    Github repos solution

--- JS-Applications/Lab/Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JS-Applications/Lab/Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,29 @@
-function loadRepos() {
-	console.log("TODO...");
+async function loadRepos() {
+  const username = document.getElementById("username").value;
+  const list = document.getElementById("repos");
+
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok == false) {
+      const message = await response.json();
+      throw message;
+    }
+    const data = await response.json();
+    console.log(data);
+
+    list.replaceChildren();
+    data.forEach((repo) => {
+      const link = document.createElement("a");
+      link.href = repo.html_url;
+      link.textContent = repo.full_name;
+
+      const listItem = document.createElement("li");
+      listItem.appendChild(link);
+      list.appendChild(listItem);
+    });
+  } catch (error) {
+    list.textContent = error.message;
+  }
 }
