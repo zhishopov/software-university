8f9e097c1dc44dab876f9942f0abf6f0da8b4104
Zhivko Shopov <zhishopov@gmail.com>
2025-01-01 17:00:41 +0000

    Github commits solution

--- JS-Applications/Lab/Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS-Applications/Lab/Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,28 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const username = document.getElementById("username").value;
+  const repo = document.getElementById("repo").value;
+  const commits = document.getElementById("commits");
+
+  const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok == false) {
+      const message = await response.json();
+      throw message;
+    }
+    const data = await response.json();
+
+    commits.replaceChildren();
+    data.forEach((entry) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${entry.commit.author.name}: ${entry.commit.message}`;
+      commits.appendChild(listItem);
+    });
+  } catch (error) {
+    commits.replaceChildren();
+    const listItem = document.createElement("li");
+    listItem.textContent = `Error: ${error.status} (Not Found)`;
+    commits.appendChild(listItem);
+  }
 }
