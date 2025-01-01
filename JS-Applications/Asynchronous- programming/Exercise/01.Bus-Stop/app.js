bb26e022a0b3a142823bced52c84667f74c6e841
Zhivko Shopov <zhishopov@gmail.com>
2025-01-01 20:36:10 +0000

    Bus Stop solution

--- JS-Applications/Asynchronous- programming/Exercise/01.Bus-Stop/app.js
+++ JS-Applications/Asynchronous- programming/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,27 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const stopId = document.getElementById("stopId").value;
+  const stopName = document.getElementById("stopName");
+  const busesList = document.getElementById("buses");
+
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+  busesList.replaceChildren();
+  try {
+    const response = await fetch(url);
+
+    if (response.ok == false) {
+      const errorMessage = await response.json();
+      throw errorMessage;
+    }
+
+    const data = await response.json();
+    stopName.textContent = data.name;
+
+    for (let [busId, time] of Object.entries(data.buses)) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Bus ${busId} arrives in ${time} minutes`;
+      busesList.appendChild(listItem);
+    }
+  } catch (error) {
+    stopName.textContent = "Error";
+  }
 }
