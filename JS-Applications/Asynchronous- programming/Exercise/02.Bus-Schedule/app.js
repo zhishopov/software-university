4fd8b2900d8620718d79f2096b1e9f350a53dfff
Zhivko Shopov <zhishopov@gmail.com>
2025-01-02 10:43:15 +0000

    Bus Schedule solution

--- JS-Applications/Asynchronous- programming/Exercise/02.Bus-Schedule/app.js
+++ JS-Applications/Asynchronous- programming/Exercise/02.Bus-Schedule/app.js
@@ -1,17 +1,48 @@
 function solve() {
+  let departBtn = document.getElementById("depart");
+  let arriveBtn = document.getElementById("arrive");
+  const infoBox = document.querySelector(".info");
 
-    function depart() {
-        console.log('Depart TODO...');
+  let nextStopId = "depot";
+  let stopName = "";
+
+  async function depart() {
+    const url = `http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`;
+    try {
+      const response = await fetch(url);
+
+      if (response.ok == false) {
+        const message = await response.json();
+        throw message;
+      }
+
+      const data = await response.json();
+      console.log(data);
+      nextStopId = data.next;
+      stopName = data.name;
+
+      infoBox.textContent = `Next stop ${stopName}`;
+
+      departBtn.disabled = true;
+      arriveBtn.disabled = false;
+    } catch (error) {
+      infoBox.textContent = "Error";
+      departBtn.disabled = true;
+      arriveBtn.disabled = true;
     }
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function arrive() {
+    infoBox.textContent = `Arriving at ${stopName}`;
 
-    return {
-        depart,
-        arrive
-    };
+    arriveBtn.disabled = true;
+    departBtn.disabled = false;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
