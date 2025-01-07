39e75530844e854be0571f1068c47b0ff8f91bcb
Zhivko Shopov <zhishopov@gmail.com>
2025-01-07 16:18:42 +0000

    Handling url and simple routing

--- JS-Backend/http-server-demo/server.js
+++ JS-Backend/http-server-demo/server.js
@@ -3,7 +3,16 @@ import http from "http";
 const server = http.createServer((request, response) => {
   console.log("HTTP Request");
 
-  response.write("Hello world");
+  // Get url from request
+  const url = request.url;
+
+  // Simple Routing
+  if (url === "/") {
+    response.write("Hello World!");
+  } else if (url === "/cats") {
+    response.write("Cats Page!");
+  }
+
   response.end();
 });
 
