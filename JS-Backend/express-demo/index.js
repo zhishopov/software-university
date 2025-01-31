d47882168c15a9cde496449d842cab0682ca20ce
Zhivko Shopov <zhishopov@gmail.com>
2025-01-31 16:33:09 +0000

    Working with params

--- JS-Backend/express-demo/index.js
+++ JS-Backend/express-demo/index.js
@@ -10,6 +10,13 @@ app.get("/cats", (request, response) => {
   response.send("Cats Page!");
 });
 
+// Paths can have params
+// Express automatically stores the param in the request as a object
+app.get("/cats/:catId", (request, response) => {
+  const catId = request.params.catId;
+  response.send(`Cat Page | ${catId}`);
+});
+
 app.listen(3000, () =>
   console.log("Server is listening on http://localhost:3000...")
 );
