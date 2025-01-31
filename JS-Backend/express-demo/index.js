225879a0f3b21beafd955daf68c32ad1feffc1d9
Zhivko Shopov <zhishopov@gmail.com>
2025-01-31 17:08:34 +0000

    Using redirect

--- JS-Backend/express-demo/index.js
+++ JS-Backend/express-demo/index.js
@@ -11,6 +11,10 @@ app.get("/cats", (request, response) => {
   response.send("Cats Page!");
 });
 
+app.get("*", (request, response) => {
+  response.send("Page Not Found!");
+});
+
 // Paths can have params
 // Express automatically stores the param in the request as a object
 app.get("/cats/:catId", (request, response) => {
@@ -27,11 +31,21 @@ app.get("/download2", (requset, response) => {
   response.sendFile(path.resolve("./cat.jpeg"));
 });
 
+// Working with json
 app.get("/data", (request, response) => {
   response.json({
     name: "Data",
     grades: [1, 2, 3, 4, 5],
   });
+});
+
+// Using redirect
+app.get("/redirect", (request, response) => {
+  if (Math.random() < 0.5) {
+    response.redirect("/");
+  } else {
+    response.redirect("/404");
+  }
 });
 
 app.listen(3000, () =>
